20d03c343659f435630bde44f3af30b12ee636c0
James E <jahhmez@gmail.com>
2023-12-16 19:17:11 -0500

    contact url fixes

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -81,7 +81,7 @@ export default function ContactForm() {
     setDisabled(true);
     if (validateForm()) {
       enqueueSnackbar("It's on it's way, hang tight!", { variant: "info" });
-      fetch("/contact", {
+      fetch("https://galacticdesign/api/galactic-design/contact", {
         method: "POST",
         body: JSON.stringify({
           name: name,
